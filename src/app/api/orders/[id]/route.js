e23541db9274b8a72cd29dbc2e014ddf76268765
Simon Okbagergis Manna <32343117+saimenogm@users.noreply.github.com>
2025-03-22 15:14:39 +0300

    second-commit11

--- src/app/api/orders/[id]/route.js
+++ src/app/api/orders/[id]/route.js
@@ -1,6 +1,6 @@
 // app/api/orders/[id]/route.js
 import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/server"; // Adjust path if needed
+import { createClient } from "../../../lib/supabase/server"; // Adjust path if needed
 
 export async function GET(request, { params }) {
   try {
